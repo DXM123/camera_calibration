7a9434ad81ef40a89cdf9c442808a49306d7bbf5
Robin Schneider <robinschneider93@gmail.com>
2024-01-28 10:45:50 +0100

    Change name to PascalCase

---
+++ src/falcons/DeWarp/__init__.py
@@ -0,0 +1,14 @@
+# import sys
+
+# from PyQt5.QtWidgets import QApplication
+
+# from .application import CamCalMain
+
+__version__: str = "0.0.0"
+
+
+# if __name__ == "__main__":
+#     app = QApplication(sys.argv)
+#     ex = CamCalMain()
+#     ex.show()
+#     sys.exit(app.exec_())
