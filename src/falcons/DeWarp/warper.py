289ca62d546a0ed8c1f35be3171e34ee35c51bd1
Facilis <r.mielen@gmail.com>
2024-03-30 13:26:02 +0100

    Adding Simple Grid for pwarp tweaking

--- src/falcons/DeWarp/warper.py
+++ src/falcons/DeWarp/warper.py
@@ -44,6 +44,67 @@ class Warper(object):
             self.interpolation = cv2.INTER_CUBIC
         else:
             self.interpolation = interpolation
+
+    # Now only Basic grid, needs to be more intelligent
+    # frame, Hv=M, boundaries_rcs_mm = ??
+    def draw_grid(self, img):
+        grid_size = 50  # Grid size in pixels
+        
+        # Draw vertical Green lines
+        for x in range(0, img.shape[1], grid_size):
+            cv2.line(img, (x, 0), (x, img.shape[0]), color=(255, 255, 255), thickness=1)
+        
+        # Draw horizontal Green lines
+        for y in range(0, img.shape[0], grid_size):
+            cv2.line(img, (0, y), (img.shape[1], y), color=(255, 255, 255), thickness=1)
+
+    ##########################################################################
+
+    def draw_grid_new(self, img, boundaries_rcs_mm=None):
+        MM = 1e-3  # Define MM as per the previous context
+        MX, MY = 10, 10
+        Hv = self.M  # Using the homography matrix of the Warper instance
+        
+        # Green 1m-per-square grid
+        for rx in range(-MX, MX + 1):
+            iPts = np.array([[rx / MM, 0], [rx / MM, MY / MM]], dtype=np.float32)
+            oPts = cv2.perspectiveTransform(np.array([iPts]), Hv)[0]
+            cv2.line(img, tuple(oPts[0]), tuple(oPts[1]), (0, 255, 0), 1)
+        
+        for ry in range(MY + 1):
+            iPts = np.array([[-MX / MM, ry / MM], [MX / MM, ry / MM]], dtype=np.float32)
+            oPts = cv2.perspectiveTransform(np.array([iPts]), Hv)[0]
+            cv2.line(img, tuple(oPts[0]), tuple(oPts[1]), (0, 255, 0), 1)
+        
+        # Red diagonals
+        for sign in [-1, 1]:
+            iPts = np.array([[sign * MX / MM, MY / MM], [0, 0]], dtype=np.float32)
+            oPts = cv2.perspectiveTransform(np.array([iPts]), Hv)[0]
+            cv2.line(img, tuple(oPts[0]), tuple(oPts[1]), (255, 0, 0), 1)
+        
+        # Red field boundaries (if provided)
+        if boundaries_rcs_mm is not None:
+            for i in range(1, len(boundaries_rcs_mm)):
+                iPts = np.array([
+                    [boundaries_rcs_mm[i].x, boundaries_rcs_mm[i].y],
+                    [boundaries_rcs_mm[i-1].x, boundaries_rcs_mm[i-1].y]
+                ], dtype=np.float32)
+                oPts = cv2.perspectiveTransform(np.array([iPts]), Hv)[0]
+                cv2.line(img, tuple(oPts[0]), tuple(oPts[1]), (255, 0, 0), 1)
+    
+    ##########################################################################
+
+    def rotate_image(self, img, angle):
+        # Compute the center of the image
+        center = (img.shape[1] // 2, img.shape[0] // 2)
+
+        # Compute the rotation matrix
+        M = cv2.getRotationMatrix2D(center, angle, scale=1.0)
+
+        # Perform the rotation
+        rotated_img = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+        return rotated_img
+
             
     def warp(self, img):
         # Determine if resizing is necessary based on supersample value
@@ -69,7 +130,14 @@ class Warper(object):
             # No resizing needed, use the warped image directly -> But odd output
             result_img = self.dst
 
+        # Rotate the resulting image by 45 degrees
+        result_img = self.rotate_image(result_img, angle=-45)
+
         # Print the actual resulting image size
         print(f"Resulting image size: width={result_img.shape[1]}, height={result_img.shape[0]}")
+
+        #Plot Grid here ? TODO
+        # Draw grid on the resulting image
+        self.draw_grid(result_img)
 
         return result_img
