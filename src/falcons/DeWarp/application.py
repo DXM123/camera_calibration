28e49797d71d17e52f5a714f258e7b36d72606ab
FLoock <florent.loock@gmail.com>
2024-07-11 20:30:22 +0200

    split calibration in 4 classes, add layout for chessboard parameters

--- src/falcons/DeWarp/application.py
+++ src/falcons/DeWarp/application.py
@@ -33,7 +33,7 @@ class CamCalMain(QMainWindow):
         # Setup input video (or test) stream
         if video is None:
             video = 0 # default camera
-        self.video_input = VideoInput(video)
+        # self.video_input = VideoInput(video)
 
         # Initialize camera_matrix as None
         #self.camera_matrix = None
@@ -95,53 +95,55 @@ class CamCalMain(QMainWindow):
             self, "Select LUT binary File", "", "BIN Files (*.bin);;All Files (*)", options=options
         )
 
-        if file_name:
-            try:
-                with open(file_name, "r") as f:
-                    self.camera_widget.lut = self.camera_widget.load_lut_from_binary(file_name)
-                    self.camera_widget.lut_imported = True
+        if file_name == None:
+            return
+
+        try:
+            with open(file_name, "r") as f:
+                self.camera_widget.lut = self.camera_widget.load_lut_from_binary(file_name)
+                self.camera_widget.lut_imported = True
 
 
-                # First, disconnect all previously connected signals to avoid multiple connections.
-                try:
-                    self.camera_widget.startButtonPwarp.clicked.disconnect()
-                except TypeError:
-                    # If no connections exist, a TypeError is raised. Pass in this case.
-                    pass
+            # # First, disconnect all previously connected signals to avoid multiple connections.
+            # try:
+            #     self.camera_widget.startButtonPwarp.clicked.disconnect()
+            # except TypeError:
+            #     # If no connections exist, a TypeError is raised. Pass in this case.
+            #     pass
 
-                #Move to verify LUT when manually imported
-                self.camera_widget.startButtonPwarp.setEnabled(True)
-                self.camera_widget.startButtonPwarp.setText("Verify Lookup Table (LUT)")
-                self.camera_widget.startButtonPwarp.clicked.connect(self.camera_widget.verify_lut)  # close when done
+            #Move to verify LUT when manually imported
+            # self.camera_widget.startButtonPwarp.setEnabled(True)
+            # self.camera_widget.startButtonPwarp.setText("Verify Lookup Table (LUT)")
+            # self.camera_widget.startButtonPwarp.clicked.connect(self.camera_widget.verify_lut)  # close when done
 
-                #Switch to tab2 TODO
-                # Disable the first tab (Camera Calibration)
-                self.camera_widget.tabs.setTabEnabled(0, False) # Should not be here !
+            #Switch to tab2 TODO
+            # Disable the first tab (Camera Calibration)
+            # self.camera_widget.tabs.setTabEnabled(0, False) # Should not be here !
 
-                # Switch to the second tab (Perspective-Warp to test LUT)
-                self.camera_widget.tabs.setCurrentIndex(1)
+            # Switch to the second tab (Perspective-Warp to test LUT)
+            # self.camera_widget.tabs.setCurrentIndex(1)
 
-                # Dont need this is 2e tab
-                try:
-                    self.camera_widget.captureButton1.clicked.disconnect()
-                except TypeError:
-                    # If no connections exist, a TypeError is raised. Pass in this case.
-                    pass
+            # # Dont need this is 2e tab
+            # try:
+            #     self.camera_widget.captureButton1.clicked.disconnect()
+            # except TypeError:
+            #     # If no connections exist, a TypeError is raised. Pass in this case.
+            #     pass
 
-                if self.camera_widget.imageFrame.pixmap() is None:
-                    print("No Image Loaded")
-                    self.camera_widget.outputWindow.setText("No Image loaded")
-                    QMessageBox.question(
-                        self, "Warning!", f"No Image detected.\n\nPlease make sure an Image is loaded", QMessageBox.Ok)
+            # if self.camera_widget.imageFrame.pixmap() is None:
+            #     print("No Image Loaded")
+            #     self.camera_widget.outputWindow.setText("No Image loaded")
+            #     QMessageBox.question(self, "Warning!", f"No Image detected.\n\nPlease make sure an Image is loaded", QMessageBox.Ok)
+            #     return
 
 
-            except FileNotFoundError:
-                print(f"File {filename} not found.")
-                self.camera_widget.update_status_signal.emit(f"File not found: {file_name}")
+        except FileNotFoundError:
+            print(f"File {filename} not found.")
+            self.camera_widget.update_status_signal.emit(f"File not found: {file_name}")
 
-            except Exception as e:
-                print(f"Error loading calibration file: {e}")
-                self.camera_widget.update_status_signal.emit("Error loading calibration file")
+        except Exception as e:
+            print(f"Error loading calibration file: {e}")
+            self.camera_widget.update_status_signal.emit("Error loading calibration file")
 
 
     def load_calibration(self, filename):
@@ -153,58 +155,53 @@ class CamCalMain(QMainWindow):
             self, "Select Calibration File", "", "JSON Files (*.json);;All Files (*)", options=options
         )
 
-        if file_name:
-            try:
-                with open(file_name, "r") as f:
-                    data = json.load(f)
+        if file_name == None:
+            return
+        
+        try:
+            with open(file_name, "r") as f:
+                data = json.load(f)
 
-                    # Convert lists to numpy arrays
-                    self.camera_widget.camera_matrix = np.array(data.get("camera_matrix"))
-                    self.camera_widget.camera_dist_coeff = np.array(data.get("dist_coeff"))
+                self.camera_widget.camera_matrix = np.array(data.get("camera_matrix"))
+                self.camera_widget.camera_dist_coeff = np.array(data.get("dist_coeff"))
 
+                if self.camera_widget.camera_matrix.shape != (3, 3):
+                    raise ValueError("Camera matrix must be a 3x3 array.")
 
-                    # Validate shape
-                    if self.camera_widget.camera_matrix.shape != (3, 3):
-                        raise ValueError("Camera matrix must be a 3x3 array.")
+                if self.camera_widget.camera_dist_coeff.shape != (4, 1):
+                    raise ValueError("Distortion coefficients must be a 4x1 array.")
 
-                    # Validate shape of distortion coefficients if necessary
-                    if self.camera_widget.camera_dist_coeff.shape != (4, 1):
-                        raise ValueError("Distortion coefficients must be a 4x1 array.")
+                self.camera_widget.update_status_signal.emit(f"Calibration parameters loaded from {file_name}")
+                print(f"Calibration parameters loaded from {file_name}")
 
-                    # Emit the signal with the updated status text
-                    self.camera_widget.update_status_signal.emit(f"Calibration parameters loaded from {file_name}")
-                    print(f"Calibration parameters loaded from {file_name}")
+                print(f"Camera Matrix:\n{self.camera_widget.camera_matrix}")
+                print(f"Distortion Coefficients:\n{self.camera_widget.camera_dist_coeff}")
 
-                    # Debug print for camera matrix and distortion coefficients
-                    print(f"Camera Matrix:\n{self.camera_widget.camera_matrix}")
-                    print(f"Distortion Coefficients:\n{self.camera_widget.camera_dist_coeff}")
+                # Set tracker to True in camera_widget, needed in test_cam_calibration
+                self.camera_widget.cal_imported = True
 
-                    # Set tracker to True in camera_widget, needed in test_cam_calibration
-                    self.camera_widget.cal_imported = True
+                # First, disconnect all previously connected signals to avoid multiple connections.
+                try:
+                    self.camera_widget.doneButton1.clicked.disconnect()
+                except TypeError:
+                    # If no connections exist, a TypeError is raised. Pass in this case.
+                    pass
 
-                    # First, disconnect all previously connected signals to avoid multiple connections.
-                    try:
-                        self.camera_widget.doneButton1.clicked.disconnect()
-                    except TypeError:
-                        # If no connections exist, a TypeError is raised. Pass in this case.
-                        pass
+                # Update DONE button to Test Calibration -> only when tab 1 is active TODO
+                self.camera_widget.doneButton1.setText("Test Calibration")
+                self.camera_widget.doneButton1.clicked.connect(
+                    self.camera_widget.test_calibration
+                )  # change connect to calibartion test
 
-                    # Update DONE button to Test Calibration -> only when tab 1 is active TODO
-                    self.camera_widget.doneButton1.setText("Test Calibration")
-                    self.camera_widget.doneButton1.clicked.connect(
-                        self.camera_widget.test_calibration
-                    )  # change connect to calibartion test
+        except FileNotFoundError:
+            print(f"File {filename} not found.")
+            self.camera_widget.update_status_signal.emit(f"File not found: {file_name}")
 
-            except FileNotFoundError:
-                print(f"File {filename} not found.")
-                self.camera_widget.update_status_signal.emit(f"File not found: {file_name}")
-
-            except Exception as e:
-                print(f"Error loading calibration file: {e}")
-                self.camera_widget.update_status_signal.emit("Error loading calibration file")
+        except Exception as e:
+            print(f"Error loading calibration file: {e}")
+            self.camera_widget.update_status_signal.emit("Error loading calibration file")
 
     def check_tmp_data_empty(self):
-        # Check if the temporary data folder exists for temporarily processing captured images
         if not os.path.exists(self.config.tmp_data):
             try:
                 os.makedirs(self.config.tmp_data, exist_ok=True)
@@ -217,7 +214,6 @@ class CamCalMain(QMainWindow):
             existing_images = [f for f in os.listdir(self.config.tmp_data) if f.startswith("corner_") and f.endswith(".png")]
 
             if existing_images:
-                # Ask the user if they want to delete existing images
                 reply = QMessageBox.question(
                     self,
                     "Existing Images",
@@ -232,15 +228,11 @@ class CamCalMain(QMainWindow):
                         file_path = os.path.join(self.config.tmp_data, image_file)
                         os.remove(file_path)
 
-                    # Inform the user about the deletion
                     QMessageBox.information(self, "Deletion Complete", "Existing images have been deleted.", QMessageBox.Ok)
 
                 else:
-                    # If the user chooses not to delete, inform them and exit the method
                     QMessageBox.information(self, "Calibration Canceled", "Calibration process canceled.", QMessageBox.Ok)
-
-                    sys.exit(0) # Exits the application without an error status, this is by choice.
+                    sys.exit(0)
 
     def update_status_bar(self, status_text):
-        # Update the status bar text
         self.statusBar().showMessage(status_text)
--- src/falcons/DeWarp/cameraCalibration.py
+++ src/falcons/DeWarp/cameraCalibration.py
@@ -9,7 +9,7 @@ import glob # for load_images
 import math
 
 from PyQt5.QtCore import Qt, QTimer, pyqtSignal
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtGui import QImage, QPixmap, QIntValidator, QDoubleValidator
 from PyQt5.QtWidgets import (
     QApplication,
     QButtonGroup,
@@ -27,6 +27,7 @@ from PyQt5.QtWidgets import (
     QTextEdit,
     QVBoxLayout,
     QWidget,
+    QGridLayout
 )
 
 from .common import MarkerColors, SoccerFieldColors
@@ -35,14 +36,135 @@ from .soccer_field import SoccerField
 from .warper import Warper
 from .pylonThread import PylonThread
 
+class CalibrationInput():
+    class CalibrationType:
+        CAMERA = 0
+        VIDEO = 1
+        IMAGE = 2
+        PYLON = 3
+
+    def __init__(self):
+        pass
+
+    def getImage(self):
+        pass
+
+class CalibrationInputCamera(CalibrationInput):
+    def __init__(self):
+        super().__init__()
+
+    def getImage(self):
+        pass
+
+class CalibrationInputImage(CalibrationInput):
+    def __init__(self):
+        super().__init__()
+
+    def getImage(self):
+        pass
+class CalibrationInputNetwork(CalibrationInput):
+    def __init__(self):
+        super().__init__()
+
+    def getImage(self):
+        pass
+
+class CalibrationInputPylon(CalibrationInput):
+    def __init__(self):
+        super().__init__()
+
+    def getImage(self):
+        pass
+
+    
+
 class WidgetCameraCalibration(QWidget):
     def __init__(self, parent):
         super().__init__()
-        self.initGui()
         self.parent = parent
+        self.inputClass = CalibrationInputCamera()
+        self.initGui()
 
     def initGui(self):
-        pass
+        layout = QHBoxLayout()
+        
+        # display layout
+        imageDisplayLayout = QVBoxLayout()
+        imageDisplay = QLabel('Image Display')
+        layout.addWidget(imageDisplay)
+
+
+        layout.addLayout(imageDisplayLayout)
+
+
+        # settings layout
+        settingsLayout = QVBoxLayout()
+        settingsLayout.setAlignment(Qt.AlignTop)
+        settingsLayout.addWidget(QLabel('Input Options'))
+
+        # input options
+        inputTypeLayout = QHBoxLayout()
+        settingsLayout.addLayout(inputTypeLayout)
+
+        inputTypeWidget = QButtonGroup()
+
+        inputTypeCamera = QRadioButton('Camera')
+        inputTypeCamera.setChecked(True)
+        inputTypeCamera.toggled.connect(lambda: self.setInputType(CalibrationInput.CalibrationType.CAMERA))
+        inputTypeWidget.addButton(inputTypeCamera)
+        inputTypeLayout.addWidget(inputTypeCamera)
+
+
+        inputTypeImage = QRadioButton('Image')
+        inputTypeImage.toggled.connect(lambda: self.setInputType(CalibrationInput.CalibrationType.IMAGE))
+        inputTypeWidget.addButton(inputTypeImage)
+        inputTypeLayout.addWidget(inputTypeImage)
+
+        inputTypeNetwork = QRadioButton('Network')
+        inputTypeNetwork.toggled.connect(lambda: self.setInputType(CalibrationInput.CalibrationType.NETWORK))
+        inputTypeWidget.addButton(inputTypeNetwork)
+        inputTypeLayout.addWidget(inputTypeNetwork)
+
+        inputTypePylon = QRadioButton('Pylon')
+        inputTypePylon.toggled.connect(lambda: self.setInputType(CalibrationInput.CalibrationType.PYLON))
+        inputTypeWidget.addButton(inputTypePylon)
+        inputTypeLayout.addWidget(inputTypePylon)
+
+        # chessBoard parameters
+        chessBoardLayout = QGridLayout()
+        settingsLayout.addLayout(chessBoardLayout)
+
+        chessBoardLayout.addWidget(QLabel('Colums:'), 0, 0)
+        chessBoardColums = QLineEdit()
+        chessBoardColums.setValidator(QIntValidator())
+        chessBoardColums.setText("9")
+        chessBoardLayout.addWidget(chessBoardColums, 0, 1)
+
+
+        chessBoardLayout.addWidget(QLabel('Rows:'), 1, 0)
+        chessBoardRows = QLineEdit()
+        chessBoardRows.setValidator(QIntValidator())
+        chessBoardRows.setText("6")
+        chessBoardLayout.addWidget(chessBoardRows, 1, 1)
+        
+        chessBoardLayout.addWidget(QLabel('Square Size (mm):'), 2, 0)
+        chessBoardSquareSize = QLineEdit()
+        chessBoardSquareSize.setValidator(QDoubleValidator())
+        chessBoardSquareSize.setText("23.0")
+        chessBoardLayout.addWidget(chessBoardSquareSize, 2, 1)
+
+
+
+
+
+
+        layout.addLayout(settingsLayout)
+
+        self.setLayout(layout)
+
+    def setInputType(self, inputType):
+        print(inputType)
+
 
     def keyPressEvent(self, event):
         super().keyPressEvent(event)
